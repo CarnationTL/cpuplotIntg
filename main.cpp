5747b0e5760287793eba1fb307f276f2d1d45a29
CarnationTL <tongl@bjcarnation.com.cn>
2013-07-05 12:33:13 +0800

    add layout

--- main.cpp
+++ main.cpp
@@ -6,26 +6,7 @@ int main(int argc, char *argv[]) {
     MainCpuplot w;
     w.show();
     
-    QWidget vBox(&w);
-    vBox.setWindowTitle( "Cpu Plot" );
 
-    CpuPlot *plot = new CpuPlot( &vBox );
-    plot->setTitle( "History" );
-
-    const int margin = 5;
-    plot->setContentsMargins( margin, margin, margin, margin );
-
-    QString info( "Press the legend to en/disable a curve" );
-
-    QLabel *label = new QLabel( info, &vBox );
-
-    QVBoxLayout *layout = new QVBoxLayout( &vBox );
-    layout->addWidget( plot );
-    layout->addWidget( label );
-
-    vBox.resize( 600, 400 );
-    w.resize (600, 400);
-    vBox.show();
 
     return a.exec();
 }
--- maincpuplot.cpp
+++ maincpuplot.cpp
@@ -1,15 +1,45 @@
 #include "maincpuplot.h"
 #include "ui_maincpuplot.h"
+#include <QLabel>
+#include <QVBoxLayout>
 
 MainCpuplot::MainCpuplot(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainCpuplot)
 {
     ui->setupUi(this);
+
+
+    QVBoxLayout *pl = new QVBoxLayout(ui->centralWidget);
+
+
+    QWidget *vBox = new QWidget(ui->centralWidget);
+    pl->addWidget (vBox);
+
+    vBox->setWindowTitle( "Cpu Plot" );
+
+    CpuPlot *plot = new CpuPlot( vBox );
+    plot->setTitle( "History" );
+
+    const int margin = 5;
+    plot->setContentsMargins( margin, margin, margin, margin );
+
+    QString info( "Press the legend to en/disable a curve" );
+
+    QLabel *label = new QLabel( info, vBox );
+
+    QVBoxLayout *layout = new QVBoxLayout( vBox );
+    layout->addWidget( plot );
+    layout->addWidget( label );
+
+    vBox->resize( 600, 400 );
+    ui->centralWidget->resize (600, 400);
+    //vBox->show();
 }
 
 MainCpuplot::~MainCpuplot()
 {
+    //TODO release resource
     delete ui;
 }
 
